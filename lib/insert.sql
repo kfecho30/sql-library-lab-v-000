3243f9a523f2c96507a5e87f585013b180b27207
Kelsey Fecho <kfecho30@gmail.com>
2018-03-29 16:08:30 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,9 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("A History of Magic", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Space Craze", 2, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("Mysterious Mirror", 2015, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Darker Shade of Magic", 2016, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Conjuring of Magic", 2017, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Odyssey", 2013, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Landing", 2014, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Implosion", 2015, 2);

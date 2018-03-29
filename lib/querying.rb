17665ba6795ae8c22ff0bf5abe4cd36ed8a4ce98
Kelsey Fecho <kfecho30@gmail.com>
2018-03-29 16:33:51 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
-    JOIN subgenres ON authors.id = subgenres.author_id;"
+    JOIN series ON authors.id = series.author_id
+    JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
